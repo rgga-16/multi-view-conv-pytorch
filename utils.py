166ee167a204a6ec2d5e83ad28a218e1d987c4a3
rgga16 <rgga16@gmail.com>
2021-12-31 22:45:11 +0900

    added load_image function

--- utils.py
+++ utils.py
@@ -1,11 +1,20 @@
 import yaml 
 import os 
+from PIL import Image
 
 def read_configs(path='./config.yaml'):
     with open(path) as f:
         configs = yaml.load(f,Loader=yaml.FullLoader)
         f.close()
     return configs
+
+def load_image(filename,mode='RGB'):
+    img = Image.open(filename).convert(mode)
+    return img
+
+def image_to_tensor():
+
+    return tensor 
 
 
 global configs 
