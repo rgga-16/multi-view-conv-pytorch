d2bb531826592666d77126a59b179426cc8cb485
rgga16 <rgga16@gmail.com>
2022-01-04 22:10:11 +0900

    Finished debugging data loading function

--- data.py
+++ data.py
@@ -2,42 +2,46 @@ import torch
 from torch.utils.data import Dataset
 import os 
 
-from utils import configs, load_image,image_to_tensor
+from utils import configs, load_image,itot,toti
 
 class Data(Dataset):
-    def __init__(self,root,category,views,shape_list,shapenet_ids=[],shuffle=True,batch_size=-1):
+    def __init__(self,root,category,views,shape_list,shuffle=True,batch_size=-1):
 
         self.sketch_model_pairs = []
-        self.shapenet_ids = shapenet_ids
 
-        n_source_views = len(configs['SKETCH_VIEWS'])
-        # n_dnfs_views = max(2,n_source_views)
-        n_dnfs_views=2
-        n_dn_views = 12
-        n_target_views = n_dnfs_views + n_dn_views
+        self.n_source_views = len(configs['SKETCH_VIEWS'])
+        # self.n_dnfs_views = max(2,n_source_views)
+        self.n_dnfs_views=2
+        self.n_dn_views = 12
+        self.n_target_views = self.n_dnfs_views + self.n_dn_views
 
+        self.shape_list = shape_list 
         self.dn_lists = []
         self.dnfs_lists = []
         self.sketch_lists = []
        
-        self.load_files(root,shape_list,n_dn_views,n_dnfs_views)
-        print()
+        self.load_files(root,shape_list)
+
+        assert len(self.dn_lists)==len(self.dnfs_lists)
+        assert len(self.dn_lists)==len(self.sketch_lists)
+        assert len(self.dnfs_lists)==len(self.sketch_lists)
+
     
-    def load_files(self,root,shape_list,n_dn_views,n_dnfs_views):
+    def load_files(self,root,shape_list):
         imsize = configs['IMSIZE']
 
         for shape_name in shape_list:
             shape = {}
             shape['name'] = shape_name 
 
-            dn_files = [os.path.join(root,'dn',shape_name,f'dn-{imsize}-{dn_view}.png') for dn_view in range(n_dn_views)]
-            dnfs_files = [os.path.join(root,'dnfs',shape_name,f'dnfs-{imsize}-{dnfs_view}.png') for dnfs_view in range(n_dnfs_views)]
+            dn_files = [os.path.join(root,'dn',shape_name,f'dn-{imsize}-{dn_view}.png') for dn_view in range(self.n_dn_views)]
+            dnfs_files = [os.path.join(root,'dnfs',shape_name,f'dnfs-{imsize}-{dnfs_view}.png') for dnfs_view in range(self.n_dnfs_views)]
             sketch_files = [os.path.join(root,'sketch',shape_name,f'sketch-{view}-{var}.png') for view in configs['SKETCH_VIEWS'] for var in range(configs['SKETCH_VARIATIONS'])]
             
-            dn_list = [image_to_tensor(load_image(dn_f)) for dn_f in dn_files]
-            dnfs_list = [image_to_tensor(load_image(dnfs_f)) for dnfs_f in dnfs_files]
-            sketch_list = [image_to_tensor(load_image(sketch_f,'L')) for sketch_f in sketch_files]
-        
+            dn_list = [itot(load_image(dn_f)) for dn_f in dn_files]
+            dnfs_list = [itot(load_image(dnfs_f)) for dnfs_f in dnfs_files]
+            sketch_list = torch.stack([itot(load_image(sketch_f,'L')) for sketch_f in sketch_files],dim=1)
+
             self.dn_lists.append(dn_list)
             self.dnfs_lists.append(dnfs_list)
             self.sketch_lists.append(sketch_list)
@@ -45,12 +49,10 @@ class Data(Dataset):
     
 
     def __len__(self):
-
-        return 
+        return len(self.sketch_lists)
     
     def __getitem__(self,index):
-
-        return 
+        return self.sketch_lists[index],self.dn_lists[index],self.dnfs_lists[index]
 
 def load_train_data(root,category,views,batch_size=-1):
 
@@ -58,16 +60,9 @@ def load_train_data(root,category,views,batch_size=-1):
     shape_list = shape_list_file.read().splitlines()
     shape_list_file.close() 
 
-    if category=='Airplane':
-        shapenet_ids = ['02691156']
-    elif category=='Chair':
-        shapenet_ids = ['03001627','04256520']
-    elif category=='Character':
-        shapenet_ids=[]
-    else:
-        shapenet_ids=[]
-        # raise ValueError('Category should be either Airplane, Character, or Chair (case-sensitive).')
-    return Data(root,category,views,shape_list,shapenet_ids,batch_size=batch_size)
+    assert category in ['Airplane','Chair','Character']
+
+    return Data(root,category,views,shape_list,batch_size=batch_size)
 
 def load_val_data(root,category,views,batch_size=-1):
     return 
@@ -77,7 +72,6 @@ def load_test_data(root,category,style,views,batch_size=-1):
     assert style=='Draw' or style=='Synthetic'
 
     shape_list_file = None 
-
 
     # shape_list_file = open(os.path.join(root,'train-list.txt'))
     # shape_list = shape_list_file.read().splitlines()
@@ -104,4 +98,6 @@ if __name__=='__main__':
     root = os.path.join(configs['TRAIN_DIR'],configs['CATEGORY'])
     
     train_data = load_train_data(root,configs['CATEGORY'],None)
+
+    sketches,dns,dnfss = train_data.__getitem__(0)
     print()
