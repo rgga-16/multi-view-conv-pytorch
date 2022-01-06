5ad65783832d845d4c0dd9eee88ff4b41bbf457d
rgga16 <rgga16@gmail.com>
2022-01-06 19:56:49 +0900

    started adding loss functions

--- ops.py
+++ ops.py
@@ -1,1 +1,28 @@
 # Computation of losses and other operations here
+import torch
+
+def overall_loss():
+
+    return 
+
+'''
+Per-pixel depth_loss 
+'''
+def depth_loss(pred,real):
+    l1_dist = torch.abs(pred-real)
+    return 
+
+'''
+Per-pixel normal_loss 
+'''
+def normal_loss(pred,real):
+    l1_dist = torch.abs(pred-real)
+    return 
+
+def mask_loss(pred,real):
+
+    return 
+
+def adversarial_loss(pred,real):
+
+    return 
