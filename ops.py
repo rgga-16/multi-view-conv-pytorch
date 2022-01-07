f0b9ed074f2e1d079f5724a14c411692992050b0
rgga16 <rgga16@gmail.com>
2022-01-07 17:32:05 +0900

    implemented depth, normal, and mask loss fns (mask loss is buggy) preprocessed targets and preds

--- ops.py
+++ ops.py
@@ -1,5 +1,7 @@
 # Computation of losses and other operations here
 import torch
+import torch.nn.functional as F
+from utils import device
 
 def overall_loss():
 
@@ -8,21 +10,71 @@ def overall_loss():
 '''
 Per-pixel depth_loss 
 '''
-def depth_loss(pred,real):
-    l1_dist = torch.abs(pred-real)
-    return 
+def depth_loss(pred,target,bool_mask,normalized=True):
+    """
+    input:
+        predicts   : n x H x W x 1      predicted depths
+        targets    : n x H x W x 1      ground-truth depths
+        mask       : n x H x W x 1     bool mask, included to only account for losses on foreground
+        normalized : boolean        whether output loss should be normalized by pixel number
+    output:
+        loss       : scalar             loss value
+	"""
+
+    b,c,_,_ = pred.shape
+    l1_dist = torch.abs(pred-target)
+    l1_dist_masked = torch.masked_select(l1_dist,bool_mask)
+
+    if normalized:
+        depth_loss = torch.mean(l1_dist_masked) * (b*c)
+        print()
+    else: 
+        depth_loss = torch.sum(l1_dist_masked)
+
+    return depth_loss
 
 '''
 Per-pixel normal_loss 
 '''
-def normal_loss(pred,real):
-    l1_dist = torch.abs(pred-real)
-    return 
+def normal_loss(pred,target,bool_mask,normalized=True):
+    """
+		input:
+			predicts   : n x H x W x 3      predicted normals
+			targets    : n x H x W x 3      ground-truth normals
+			mask       : n x H x W x 1      boolean mask. Applied to only account for loss values from foreground
+			normalized : boolean            whether output loss should be normalized by pixel number
+		output:
+			loss       : scalar             loss value
+	"""
 
-def mask_loss(pred,real):
+    b,c,_,_ = pred.shape 
+    l2_dist = torch.square(pred-target)
+    l2_dist_masked = torch.masked_select(l2_dist,bool_mask)
 
-    return 
+    if normalized:
+        normal_loss = torch.mean(l2_dist_masked) * (b*c)
+    else: 
+        normal_loss = torch.sum(l2_dist_masked)
 
-def adversarial_loss(pred,real):
+    return normal_loss
+
+def mask_loss(pred,target,normalized=True):
+    bce = torch.nn.BCELoss()
+    # mask_loss = F.binary_cross_entropy(pred,target)
+    mask_loss = bce(pred,target).item() #CUDA ERROR HERE. Try to debug this.
+
+    p = pred*0.5 + 0.5
+    t = target*0.5 + 0.5
+
+    # mask_loss2 = -t * log(p) - (1-t)*(log(1-p))
+    mask_loss2 = torch.sum(-1.0 * torch.multiply(torch.log(torch.max(torch.tensor(1e-6),p)),t) - torch.multiply(1-t, torch.log(torch.max(torch.tensor(1e-6),1-p))))
+
+    if normalized:
+        b,c,h,w = pred.shape
+        n_pixels = c*h*w 
+        mask_loss/=n_pixels
+    return mask_loss
+
+def adversarial_loss(pred,target):
 
     return 
--- train.py
+++ train.py
@@ -6,7 +6,8 @@ import os
 import data, network, utils,seeder 
 
 from seeder import init_fn
-from utils import configs
+from utils import configs,device,apply_mask,show_images,real_to_bool_mask
+from ops import depth_loss,normal_loss,adversarial_loss,mask_loss,overall_loss
 
 
 def train_step(model,dataloader,loss_history,optim=None):
@@ -36,11 +37,42 @@ def train_loop(model, train_data, val_data):
                 dataloader = val_loader
 
             for i,sample in enumerate(dataloader):
-                input = sample[0]; target = sample[1]
+                input_sketches = sample[0]; target_raw = sample[1].view((sample[1].shape[0]*sample[1].shape[1],sample[1].shape[2],sample[1].shape[3],-1))
 
-                print()
                 with torch.set_grad_enabled(phase=='train'):
-                    output = model(input)
+                    pred_raw = model(input_sketches)
+                    pred_content = pred_raw[:,:-1,:,:]
+                    pred_mask = pred_raw[:,-1:,:,:]
+                    preds = apply_mask(pred_content,pred_mask)
+
+                    target_content = target_raw[:,:-1,:,:]             
+                    target_mask = target_raw[:,-1:,:,:]
+                    mask_max = torch.max(target_mask)
+                    mask_min = torch.min(target_mask)
+                    targets = apply_mask(target_content,target_mask)
+
+                    if configs['PREDICT_NORMAL']:
+                        preds_normal = pred_content[:,:-1,:,:]
+                        preds_depth = pred_content[:,-1:,:,:]
+                        targets_normal = target_content[:,:-1,:,:]
+                        targets_depth = target_content[:,-1:,:,:]
+                        pass 
+                    else: 
+                        preds_depth = pred_content
+                        preds_normal = torch.tile(torch.zeros_like(preds_depth,device=device),(1,3,1,1))
+                        targets_depth = target_content
+                        targets_normal = torch.tile(torch.zeros_like(targets_depth,device=device),(1,3,1,1))
+
+                    d_loss = depth_loss(preds_depth,targets_depth,real_to_bool_mask(target_mask))
+                    n_loss = normal_loss(preds_normal,targets_normal,real_to_bool_mask(target_mask))
+                    m_loss = mask_loss(pred_mask,target_mask)
+
+                    show_images(target_content)
+                    show_images(target_mask)
+                    show_images(targets)
+                    print()
+
+
 
                     # Predicted output is the first n_channel layers. last channel is mask. 
                     # preds_content = tf.slice(preds, [0,0,0,0], [-1,-1,-1,num_channels-1])
@@ -60,9 +92,10 @@ if __name__=='__main__':
     
     train_data = data.load_train_data(root,configs['CATEGORY'],None)
 
-    n_sketch_views = len(configs['SKETCH_VIEWS']) * configs['SKETCH_VARIATIONS']
+    # n_sketch_views = len(configs['SKETCH_VIEWS']) * configs['SKETCH_VARIATIONS']
+    in_c = len(configs['SKETCH_VIEWS'])*2
 
-    model = network.MonsterNet(n_sketch_views=n_sketch_views,n_dn_views=configs['NUM_DN_VIEWS'])
+    model = network.MonsterNet(n_target_views=configs['NUM_DN_VIEWS']+configs['NUM_DNFS_VIEWS'],in_c=in_c)
 
     train_loop(model,train_data,None)
 
