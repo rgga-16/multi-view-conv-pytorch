eadecf4ba89995e4d18b5501b47f21ef5bdeb52c
rgga16 <rgga16@gmail.com>
2022-01-13 17:04:40 +0900

    Made network smaller. Removed last layer and first layer from encoder and decoder, respectively.

--- network.py
+++ network.py
@@ -22,27 +22,28 @@ class Encoder(nn.Module):
         self.e4 = self.encoder_layer(256,512)
         self.e5 = self.encoder_layer(512,512)
         self.e6 = self.encoder_layer(512,512)
-        self.e7 = self.encoder_layer(512,512)
+        # self.e7 = self.encoder_layer(512,512)
 
 
     def forward(self,input):
-        assert input.shape[1]==self.in_c #Assume input=(b,1,256,256)
+        assert input.shape[1]==self.in_c #Assume input=(b,1,256,256) 128
         
         layer_feats = {}
-        ef1 = self.e1(input) #ef1 = (b,64,128,128)
+        ef1 = self.e1(input) #ef1 = (b,64,128,128) 64
         layer_feats['ef1']=ef1 
-        ef2 = self.e2(ef1) #ef2 = (b,128,64,64)
+        ef2 = self.e2(ef1) #ef2 = (b,128,64,64) 32
         layer_feats['ef2']=ef2
-        ef3 = self.e3(ef2) #ef3 = (b,256,32,32)
+        ef3 = self.e3(ef2) #ef3 = (b,256,32,32) 16
         layer_feats['ef3']=ef3
-        ef4 = self.e4(ef3) #ef4 = (b,512,16,16)
+        ef4 = self.e4(ef3) #ef4 = (b,512,16,16) 8 
         layer_feats['ef4']=ef4
-        ef5 = self.e5(ef4) #ef5 = (b,512,8,8) 
+        ef5 = self.e5(ef4) #ef5 = (b,512,8,8) 4
         layer_feats['ef5']=ef5
-        ef6 = self.e6(ef5) #ef6 = (b,512,4,4)
+        ef6 = self.e6(ef5) #ef6 = (b,512,4,4) 2
         layer_feats['ef6']=ef6
-        ef7 = self.e7(ef6) #ef7 = (b,512,2,2)
-        layer_feats['ef7']=ef7
+        # ef7 = self.e7(ef6) #ef7 = (b,512,2,2)
+        # layer_feats['ef7']=ef7
+        ef7=ef6
         return ef7,layer_feats
 
 
@@ -64,7 +65,7 @@ class Decoder(nn.Module):
 
     def __init__(self,out_c=5):
         super(Decoder,self).__init__()
-        self.l7=self.DecoderLayer(512,512)
+        # self.l7=self.DecoderLayer(512,512)
         self.l6=self.DecoderLayer(1024,512)
         self.l5 = self.DecoderLayer(1024,512)
         self.l4 = self.DecoderLayer(1024,256)
@@ -76,13 +77,14 @@ class Decoder(nn.Module):
 
     def forward(self,ef7,layer_feats):
         assert ef7.shape[1]==512
-        df6 = F.dropout(self.l7(ef7)) # df6=(b,512,4,4)
-        df5 = F.dropout(self.l6(torch.cat((df6,layer_feats['ef6']),dim=1))) # df5=(b,512,8,8)
-        df4 = F.dropout(self.l5(torch.cat((df5,layer_feats['ef5']),dim=1))) # df4=(b,512,16,16)
-        df3 = self.l4(torch.cat((df4,layer_feats['ef4']),dim=1)) #df3=(b,256,32,32)
-        df2 = self.l3(torch.cat((df3,layer_feats['ef3']),dim=1)) #df2=(b,128,64,64)
-        df1 = self.l2(torch.cat((df2,layer_feats['ef2']),dim=1)) #df1=(b,64,128,128)
-        output = self.tanh(self.l1(torch.cat((df1,layer_feats['ef1']),dim=1))) #output=(b,5,256,256)
+        # df6 = F.dropout(self.l7(ef7)) # df6=(b,512,4,4)
+        df6=ef7
+        df5 = F.dropout(self.l6(torch.cat((df6,layer_feats['ef6']),dim=1))) # df5=(b,512,8,8) 4
+        df4 = F.dropout(self.l5(torch.cat((df5,layer_feats['ef5']),dim=1))) # df4=(b,512,16,16) 8
+        df3 = self.l4(torch.cat((df4,layer_feats['ef4']),dim=1)) #df3=(b,256,32,32) 16
+        df2 = self.l3(torch.cat((df3,layer_feats['ef3']),dim=1)) #df2=(b,128,64,64) 32
+        df1 = self.l2(torch.cat((df2,layer_feats['ef2']),dim=1)) #df1=(b,64,128,128) 64
+        output = self.tanh(self.l1(torch.cat((df1,layer_feats['ef1']),dim=1))) #output=(b,5,256,256) 128
 
         return output
 
@@ -90,20 +92,19 @@ class Decoder(nn.Module):
 class MonsterNet(nn.Module):
     def __init__(self,n_target_views,in_c=4,out_c=5):
         super(MonsterNet,self).__init__()
-
         self.encoder = Encoder(in_c=in_c).to(device)
         self.decoders = [Decoder(out_c=out_c).to(device) for _ in range(n_target_views)]
+
     
 
     def forward(self,input):
 
         views = []
         last_layer_feat,feats = self.encoder(input)
-
         for dec in self.decoders:
             views.append(dec(last_layer_feat,feats))
         pred = torch.stack(views,dim=0)
-        pred = pred.reshape(pred.shape[0]*pred.shape[1],pred.shape[2],pred.shape[3],pred.shape[4])
+        pred = torch.reshape(pred,(pred.shape[0]*pred.shape[1],pred.shape[2],pred.shape[3],pred.shape[4]))
         return pred
 
 
@@ -116,8 +117,8 @@ class Discriminator(nn.Module):
         self.sigmoid = nn.Sigmoid().to(device)
     
     def forward(self,input):
-        features,_ = self.encoder(input)
-        flat_feats = torch.flatten(features,start_dim=1)
+        last_layer_feat,_ = self.encoder(input)
+        flat_feats = torch.flatten(last_layer_feat,start_dim=1)
 
 
         probs = self.sigmoid(self.fc_layer(flat_feats)).squeeze()
--- train.py
+++ train.py
@@ -50,7 +50,7 @@ def train_loop(generator, train_data, val_data,discriminator=None,gen_chkpt_path
     lambda_imloss = 1.0
     lambda_advloss = 0.01
 
-    if gen_chkpt_path and disc_chkpt_path:
+    if gen_chkpt_path:
         gen_chkpt = torch.load(gen_chkpt_path)
         generator.load_state_dict(gen_chkpt)
         start_epoch = gen_chkpt['epoch']+1
@@ -61,7 +61,9 @@ def train_loop(generator, train_data, val_data,discriminator=None,gen_chkpt_path
         train_gen_lh = gen_chkpt['train_gen_lh']
         val_gen_lh = gen_chkpt['val_gen_lh']
         # gen_epoch_lh = gen_chkpt['gen_epoch_lh']
-
+        print(f'Generator checkpoint found. Resuming training from epoch {start_epoch+1}.\n')
+    
+    if discriminator and disc_chkpt_path:
         disc_chkpt = torch.load(disc_chkpt_path)
         discriminator.load_state_dict(disc_chkpt)
 
@@ -70,8 +72,9 @@ def train_loop(generator, train_data, val_data,discriminator=None,gen_chkpt_path
         train_disc_lh = disc_chkpt['train_disc_lh']
         val_disc_lh = disc_chkpt['val_disc_lh']
         # disc_epoch_lh = disc_chkpt['disc_epoch_lh']
+        print(f'Discriminator checkpoint found.')
 
-        print(f'Generator & Discriminator checkpoints found. Resuming training from epoch {start_epoch+1}.\n')
+        
 
 
     for epoch in range(start_epoch,n_epochs):
@@ -93,119 +96,101 @@ def train_loop(generator, train_data, val_data,discriminator=None,gen_chkpt_path
             gen_running_loss, gen_batch_running_loss = 0.0,0.0
             disc_running_loss, disc_batch_running_loss = 0.0,0.0
             for i,sample in enumerate(dataloader):
-                input_sketches = sample[0]
-                # target_raw = sample[1].view((sample[1].shape[0]*sample[1].shape[1],sample[1].shape[2],sample[1].shape[3],-1))
-                target_raw = sample[1]
-                tb,tn_views,tc,th,tw = target_raw.shape
-                target_raw = target_raw.view(tb*tn_views,tc,th,tw)
-                gen_optimizer.zero_grad()
-                if discriminator: disc_optimizer.zero_grad()
+                with torch.autograd.set_detect_anomaly(True):
+                    input_sketches = sample[0]
+                    # target_raw = sample[1].view((sample[1].shape[0]*sample[1].shape[1],sample[1].shape[2],sample[1].shape[3],-1))
+                    target_raw = sample[1]
+                    tb,tn_views,tc,th,tw = target_raw.shape
+                    target_raw = target_raw.view(tb*tn_views,tc,th,tw)
+                    gen_optimizer.zero_grad()
+                    if discriminator: disc_optimizer.zero_grad()
 
-                with torch.set_grad_enabled(phase=='train'):
-                    pred_raw = generator(input_sketches)
-                    pred_content = pred_raw[:,:-1,:,:] #Get content from prediction. Content is normal maps + depth maps
-                    pred_mask = pred_raw[:,-1:,:,:] #Get mask (-1,1) from prediction, which is the last channel.
-                    preds = apply_mask(pred_content,pred_mask) #Apply mask onto predicted content.
+                    with torch.set_grad_enabled(phase=='train'):
+                        pred_raw = generator(input_sketches)
+                        pred_content = pred_raw[:,:-1,:,:] #Get content from prediction. Content is normal maps + depth maps
+                        pred_mask = pred_raw[:,-1:,:,:] #Get mask (-1,1) from prediction, which is the last channel.
+                        preds = apply_mask(pred_content,pred_mask) #Apply mask onto predicted content.
 
-                    target_content = target_raw[:,:-1,:,:]             
-                    target_mask = target_raw[:,-1:,:,:]
-                    targets = apply_mask(target_content,target_mask)
+                        target_content = target_raw[:,:-1,:,:]             
+                        target_mask = target_raw[:,-1:,:,:]
+                        targets = apply_mask(target_content,target_mask)
 
-                    isketches_expanded = torch.tile(input_sketches,(target_raw.shape[0],1,1,1))
+                        # isketches_expanded = torch.tile(input_sketches,(target_raw.shape[0],1,1,1))
 
-                    if configs['PREDICT_NORMAL']:
-                        preds_normal = pred_content[:,:-1,:,:] #Get predicted normal maps
-                        preds_depth = pred_content[:,-1:,:,:]
-                        targets_normal = target_content[:,:-1,:,:]
-                        targets_depth = target_content[:,-1:,:,:]
-                    else: 
-                        preds_depth = pred_content
-                        preds_normal = torch.tile(torch.zeros_like(preds_depth,device=device),(1,3,1,1))
-                        targets_depth = target_content
-                        targets_normal = torch.tile(torch.zeros_like(targets_depth,device=device),(1,3,1,1))
+                        if configs['PREDICT_NORMAL']:
+                            preds_normal = pred_content[:,:-1,:,:] #Get predicted normal maps
+                            preds_depth = pred_content[:,-1:,:,:]
+                            targets_normal = target_content[:,:-1,:,:]
+                            targets_depth = target_content[:,-1:,:,:]
+                        else: 
+                            preds_depth = pred_content
+                            preds_normal = torch.tile(torch.zeros_like(preds_depth,device=device),(1,3,1,1))
+                            targets_depth = target_content
+                            targets_normal = torch.tile(torch.zeros_like(targets_depth,device=device),(1,3,1,1))
 
-                    d_loss = depth_loss(preds_depth,targets_depth,real_to_bool_mask(target_mask))
-                    n_loss = normal_loss(preds_normal,targets_normal,real_to_bool_mask(target_mask))
-                    m_loss = mask_loss(pred_mask,target_mask)
+                        d_loss = depth_loss(preds_depth,targets_depth,real_to_bool_mask(target_mask))
+                        n_loss = normal_loss(preds_normal,targets_normal,real_to_bool_mask(target_mask))
+                        m_loss = mask_loss(pred_mask,target_mask)
 
-                    # if discriminator:
-                    #     loss_g_a,loss_d = adversarial_loss2(preds,targets,discriminator)
-                    #     # disc_data = torch.cat([targets,preds],dim=0)
-                    #     # se = torch.cat([isketches_expanded,isketches_expanded],dim=0)
-                    #     # disc_data = torch.cat([se,disc_data],dim=1)
-                    #     # loss_g_a,loss_d_r,loss_d_f = adversarial_loss(disc_data,discriminator)
-                    # else: 
-                    #     loss_g_a=0
-                    #     loss_d = 0
-                    #     # loss_d_r=0
-                    #     # loss_d_f=0
-                    
-                    target_probs = discriminator(targets) #Get probablities on target/real batch
-                    real_label_d = torch.full_like(target_probs,1.0,device=device)
-                    loss_d_real = F.binary_cross_entropy(target_probs,real_label_d) #Compute discriminator error on target batch that it is real;
-                    # loss_d_real.backward()
-
-                    pred_probs = discriminator(preds) #Get probabilities on predicted/fake batch
-                    fake_label_d = torch.full_like(pred_probs.detach(),0.0,device=device)
-                    loss_d_fake = F.binary_cross_entropy(pred_probs,fake_label_d) #Compute discriminator error on predicted batch that it is fake
-                    # loss_d_fake2 = F.binary_cross_entropy(1-pred_probs,real_label)
-                    # loss_d_fake.backward()
-
-                    loss_d = loss_d_real + loss_d_fake
-                    loss_d.backward(retain_graph=True)
-                    disc_optimizer.step()
-
-                    real_label_g = torch.full_like(pred_probs,1.0,device=device)
-                    pred_probs_real = discriminator(preds)
-                    loss_g = F.binary_cross_entropy(pred_probs_real,real_label_g) #Compute error on predicted batch that it is real. For the generator
+                        if discriminator:
+                            loss_g,loss_d = adversarial_loss2(preds,targets,discriminator)
+                            # disc_data = torch.cat([targets,preds],dim=0)
+                            # se = torch.cat([isketches_expanded,isketches_expanded],dim=0)
+                            # disc_data = torch.cat([se,disc_data],dim=1)
+                            # loss_g_a,loss_d_r,loss_d_f = adversarial_loss(disc_data,discriminator)
+                        else: 
+                            loss_g=0
+                            loss_d = 0
+                            # loss_d_r=0
+                            # loss_d_f=0
 
 
-                    overall_g_loss = (lambda_imloss * (d_loss+n_loss+m_loss)) + lambda_advloss * loss_g
-                    overall_g_loss.backward()
-                    gen_optimizer.step()
-                    overall_d_loss = loss_d
+                        overall_g_loss = (lambda_imloss * (d_loss+n_loss+m_loss)) + lambda_advloss * loss_g
+                        overall_d_loss = loss_d
 
-                    # if phase=='train':
-                    #     overall_g_loss.backward(retain_graph=True)
-                    #     gen_optimizer.step()
-                    #     if discriminator:
-                    #         overall_d_loss.backward() #BUG HERE. Cannot backward overall_d_loss() 'a second time'??. Tried below already. 
-                    #         disc_optimizer.step()
-                        # I think you shouldn't input the disc_data concatenated together.
-                        # Instead, input the real batch and fake batch separately into the discriminator.
-                        # Refer to DCGAN tutorial to see how adversarial losses are implemented.
-                    
-                    gen_running_loss+= overall_g_loss *tb
-                    disc_running_loss += overall_d_loss * tb
-                    gen_batch_running_loss += overall_g_loss
-                    disc_batch_running_loss += overall_d_loss
-                    
-                    gen_lh.append(overall_g_loss)
-                    disc_lh.append(overall_d_loss)
+                        if phase=='train':
+                            overall_g_loss.backward(retain_graph=True)
+                            gen_optimizer.step()
+                            if discriminator:
+                                overall_d_loss.backward()  #BUG: There is something in the code [float Tensor (512)] that's stopping this from backpropagating.
+                                disc_optimizer.step()
+                            # I think you shouldn't input the disc_data concatenated together.
+                            # Instead, input the real batch and fake batch separately into the discriminator.
+                            # Refer to DCGAN tutorial to see how adversarial losses are implemented.
+                        
+                        gen_running_loss+= overall_g_loss *tb
+                        disc_running_loss += overall_d_loss * tb
+                        gen_batch_running_loss += overall_g_loss
+                        disc_batch_running_loss += overall_d_loss
+                        
+                        gen_lh.append(overall_g_loss)
+                        if discriminator: disc_lh.append(overall_d_loss)
 
-                    # show_images(target_content)
-                    # show_images(target_mask)
-                    # show_images(targets)
-                    if i % b == b-1: 
-                        batch_str = f'[{phase} Batch {i+1}/{len(dataloader)}] Gen Loss: {gen_batch_running_loss/b:.5f} | Disc Loss: {disc_batch_running_loss/b:.5f}'
-                        gen_batch_running_loss = 0.0; disc_batch_running_loss=0.0
-                        print(batch_str)
+                        # show_images(target_content)
+                        # show_images(target_mask)
+                        # show_images(targets)
+                        if i % b == b-1: 
+                            batch_str = f'[{phase} Batch {i+1}/{len(dataloader)}] Gen Loss: {gen_batch_running_loss/b:.5f}'
+                            if discriminator: batch_str = f'{batch_str} | Disc Loss: {disc_batch_running_loss/b:.5f}'
+                            gen_batch_running_loss = 0.0; disc_batch_running_loss=0.0
+                            print(batch_str)
 
             
             gen_epoch_loss = gen_running_loss/ dataloader.dataset.__len__()
             disc_epoch_loss = disc_running_loss / dataloader.dataset.__len__() 
             print(f'{phase} Gen Loss: {gen_epoch_loss:.5f}')
-            print(f'{phase} Disc Loss: {disc_epoch_loss:.5f}')
+            if discriminator: print(f'{phase} Disc Loss: {disc_epoch_loss:.5f}')
 
             if phase =='val' and gen_epoch_loss < best_val_gen_l:
                 best_val_gen_l = gen_epoch_loss
                 best_gen_wts = copy.deepcopy(generator.state_dict())
                 print(f'Found best generator params at epoch {epoch+1}')
             
-            if phase =='val' and disc_epoch_loss < best_val_disc_l:
-                best_val_disc_l = disc_epoch_loss 
-                best_disc_wts = copy.deepcopy(discriminator.state_dict())
-                print(f'Found best discriminator params at epoch {epoch+1}')
+            if discriminator:
+                if phase =='val' and disc_epoch_loss < best_val_disc_l:
+                    best_val_disc_l = disc_epoch_loss 
+                    best_disc_wts = copy.deepcopy(discriminator.state_dict())
+                    print(f'Found best discriminator params at epoch {epoch+1}')
         
         epoch_chkpts.append(epoch)
             # gen_epoch_lh.append(gen_epoch_loss)
@@ -218,20 +203,22 @@ def train_loop(generator, train_data, val_data,discriminator=None,gen_chkpt_path
         gen_chkpt['train_gen_lh'] = train_gen_lh
         gen_chkpt['val_gen_lh'] = val_gen_lh
         gen_chkpt['epoch_chkpts'] = epoch_chkpts
-
-        disc_chkpt = discriminator.get_state_dict()
-        disc_chkpt['best_disc_weights'] = best_disc_wts
-        disc_chkpt['best_val_disc_l'] = best_val_disc_l
-        disc_chkpt['train_disc_lh'] = train_disc_lh
-        disc_chkpt['val_disc_lh'] = val_disc_lh
-
         gen_chkpt_path = os.path.join(configs['CHKPTS_DIR'],'gen_chkpt.pt')
-        disc_chkpt_path = os.path.join(configs['CHKPTS_DIR'],'disc_chkpt.pt')
-
         torch.save(gen_chkpt,gen_chkpt_path)
-        torch.save(disc_chkpt,disc_chkpt_path)
+
+        if discriminator: 
+            disc_chkpt = discriminator.get_state_dict()
+            disc_chkpt['best_disc_weights'] = best_disc_wts
+            disc_chkpt['best_val_disc_l'] = best_val_disc_l
+            disc_chkpt['train_disc_lh'] = train_disc_lh
+            disc_chkpt['val_disc_lh'] = val_disc_lh
+            disc_chkpt_path = os.path.join(configs['CHKPTS_DIR'],'disc_chkpt.pt')
+            torch.save(disc_chkpt,disc_chkpt_path)
+            print(f'Discriminator checkpoint saved in {disc_chkpt_path}')
+        
+        
         print(f'Generator checkpoint saved in {gen_chkpt_path}')
-        print(f'Discriminator checkpoint saved in {disc_chkpt_path}')
+        
         if epoch % 10 ==10-1:
             gen_lr_scheduler.step()
             if discriminator: disc_lr_scheduler.step()
@@ -253,7 +240,7 @@ if __name__=='__main__':
     # d_in_c = 4+sketch_in_c
     d_in_c = sketch_in_c
     discriminator = network.Discriminator(in_c=d_in_c)
-    train_loop(generator,train_data,val_data,discriminator)
+    train_loop(generator,train_data,val_data,None)
 
 
     print()
